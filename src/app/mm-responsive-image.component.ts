76f32d3a18b65f4c1801bc9d7479d9be2c2e90b3
Marcel Kempf <marcel.kempf93@gmail.com>
2017-06-14 08:09:29 +0200

    <U+2728> Add polyfill behavior (e.g. for IE)

--- src/app/mm-responsive-image.component.ts
+++ src/app/mm-responsive-image.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges, ElementRef, ViewChild } from '@angular/core';
 import { ResponsiveImageInterface } from './responsive-image.interface';
+declare var picturefill: any;
 
 @Component({
     selector: 'mm-responsive-image',
-    template: `<picture class="mm-responsive-image">
+    template: `<picture #picture class="mm-responsive-image">
                     <source *ngFor="let source of sources"
                             [srcset]="source.src"
                             [media]="'(min-width: ' + source.width + 'px)'">
@@ -11,6 +12,11 @@ import { ResponsiveImageInterface } from './responsive-image.interface';
                 </picture>`
 })
 export class MmResponsiveImageComponent implements OnChanges {
+
+    /**
+     * @type {ElementRef}
+     */
+    @ViewChild('picture') pictureEl: ElementRef;
 
     /**
      * The image sources
@@ -42,6 +48,21 @@ export class MmResponsiveImageComponent implements OnChanges {
 
         if (!this.fallbackSource && this.sources.length) {
             this.fallbackSource = this.sources[(this.sources.length - 1)].src;
+        }
+
+        // Use a polyfill if the component is used in a legacy browser
+        if (typeof picturefill === 'function') {
+            setTimeout(() => {
+                const sourceEls = this.pictureEl.nativeElement.querySelectorAll('source');
+                const fallbackEl = this.pictureEl.nativeElement.querySelector('img');
+
+                Array.prototype.forEach.call(sourceEls, (sourceEl, i) => {
+                    sourceEl.setAttribute('srcset', this.sources[i].src);
+                });
+                fallbackEl.setAttribute('srcset', this.fallbackSource);
+
+                picturefill({reevaluate: true});
+            });
         }
     }
 }
