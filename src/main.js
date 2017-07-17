191ba159bb4917fa530b81efcfc36b4650ba097d
Marcel Kempf <marcel.kempf93@gmail.com>
2017-07-17 13:02:52 +0200

    🐛  Dont exclude JS files

---
+++ src/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+var index_1 = require("./app/index");
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(index_1.MmResponsiveImage);
+//# sourceMappingURL=main.js.map
